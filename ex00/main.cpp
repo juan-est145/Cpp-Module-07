7edbd5c7b2f168ce491c14c9e22d806aa0e25ba3
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-11-07 10:53:35 +0100

    Tested swap function, is successful

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: juestrel <juestrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 10:37:33 by juestrel          #+#    #+#             */
-/*   Updated: 2024/11/07 10:37:47 by juestrel         ###   ########.fr       */
+/*   Updated: 2024/11/07 10:52:42 by juestrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,25 @@
 
 int main(void)
 {
+	{
+		int a = 1, b = 2;
+		std::cout << "Value of a is " << a << " and value of b is " << b << std::endl;
+		swap(a, b);
+		std::cout << "Value of a is " << a << " and value of b is " << b << std::endl;
+	}
+	std::cout << "\n";
+	{
+		float a = 1.5f, b = 60.789f;
+		std::cout << "Value of a is " << a << " and value of b is " << b << std::endl;
+		swap(a, b);
+		std::cout << "Value of a is " << a << " and value of b is " << b << std::endl;
+	}
+	std::cout << "\n";
+	{
+		std::string a = "This is a string", b = "This is another string";
+		std::cout << "Value of a is " << a << " and value of b is " << b << std::endl;
+		swap(a, b);
+		std::cout << "Value of a is " << a << " and value of b is " << b << std::endl;
+	}
 	return (0);
 }
--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -1,4 +1,14 @@
 #ifndef WHATEVER_HPP
 #define WHATEVER_HPP
 
+#include <iostream>
+#include <string>
+
+template <typename T>
+void swap(T &a, T&b)
+{
+	T temp = a;
+	a = b;
+	b = temp;
+}
 #endif
