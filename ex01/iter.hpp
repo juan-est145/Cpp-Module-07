9196b86ed9b7e16f480aab1ff51c1ca6815cc181
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-11-07 12:09:09 +0100

    ex01 is done

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -1,4 +1,24 @@
 #ifndef ITER_HPP
 #define ITER_HPP
 
+#include <iostream>
+#include <cstdlib>
+
+template <typename T>
+void iter(T *addres, size_t length, void(*func)(T&))
+{
+	for (size_t i = 0; i < length; i++)
+		func(addres[i]);
+}
+
+template <typename T>
+void printArray(T &item)
+{
+	std::cout << item << std::endl;
+}
+template <typename T>
+void exponential(T &item)
+{
+	item *= item;
+}
 #endif
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: juestrel <juestrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 11:27:45 by juestrel          #+#    #+#             */
-/*   Updated: 2024/11/07 11:28:00 by juestrel         ###   ########.fr       */
+/*   Updated: 2024/11/07 12:08:42 by juestrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,17 @@
 
 int main(void)
 {
+	{
+		int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		iter(array, sizeof(array) / sizeof(array[0]), printArray);
+		iter(array, sizeof(array) / sizeof(array[0]), exponential);
+		std::cout << "\n";
+		iter(array, sizeof(array) / sizeof(array[0]), printArray);
+	}
+
+	{
+		char string[] = "This is a test";
+		iter(string, sizeof(string) / sizeof(string[0]), printArray);		
+	}
 	return (0);
 }
